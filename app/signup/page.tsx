3dd7255848f6fe1e0b163dd7a952140b55df7b20
rachel <shwehnit14@gmail.com>
2024-07-14 15:02:01 +0630

    fix: allow empty string

---
+++ app/signup/page.tsx
@@ -0,0 +1,13 @@
+import SignUpSection from "@/containers/auth-page/signup-page/form-section/page";
+
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="w-full h-full flex  bg-white dark:bg-black">
+      <SignUpSection />
+    </main>
+  );
+};
+
+export default Page;
